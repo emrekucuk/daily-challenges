0a95eb2d01c48d69d0faf847cac512b3ce52769d
Emre Küçük <emrekucuk74@hotmail.com>
2022-08-03 10:58:11 +0300

    Changed Has Been Folders

---
+++ 01-find-prime-numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿void FindPrimeNumbers(int lowerLimit, int upperLimit)
+{
+    for (int number = lowerLimit; number < upperLimit; number++)
+    {
+        bool isPrime = true;
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+            System.Console.WriteLine($"Prime Number: {number}");
+
+    }
+}
+
+System.Console.Write($"Lower Limit: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"Upper Limit: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int lowerLimit = firstNumber;
+int upperLimit = secondNumber;
+
+// Control
+if (firstNumber > secondNumber)
+{
+    lowerLimit = secondNumber;
+    upperLimit = firstNumber;
+}
+FindPrimeNumbers(lowerLimit, upperLimit);
---
+++ 02-find-common-divisors/Program.cs
@@ -0,0 +1,38 @@
+﻿void FindCommonDivisors(int lowerLimit, int upperLimit, int firstDivisor, int secondDivisor)
+{
+    for (int number = lowerLimit; number < upperLimit; number++)
+    {
+        bool isDivisor = false;
+
+        if (number % firstDivisor == 0 && number % secondDivisor == 0)
+            isDivisor = true;
+
+        if (isDivisor)
+            System.Console.WriteLine($"Common Divisor Number: {number}");
+
+    }
+}
+
+System.Console.Write($"Lower Limit: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"Upper Limit: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"First Divisor: ");
+int firstDivisor = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"Second Divisor: ");
+int secondDivisor = Convert.ToInt32(Console.ReadLine());
+
+int lowerLimit = firstNumber;
+int upperLimit = secondNumber;
+
+// Control
+if (firstNumber > secondNumber)
+{
+    lowerLimit = secondNumber;
+    upperLimit = firstNumber;
+}
+
+FindCommonDivisors(lowerLimit, upperLimit, firstDivisor, secondDivisor);

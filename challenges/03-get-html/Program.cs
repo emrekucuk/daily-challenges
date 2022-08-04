4342a60e8ed3568f285ce0b21ac7807f572a1de1
Emre Küçük <emrekucuk74@hotmail.com>
2022-08-04 09:28:57 +0300

    Get-Html: Created New Project

---
+++ challenges/03-get-html/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Net;
+using System.IO;
+
+
+static string GetSourceCode(string url)
+{
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+    using (StreamReader sRead = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+    {
+        return sRead.ReadToEnd();
+    }
+}
+
+string url = "https://bigpara.hurriyet.com.tr/doviz/";
+
+string document = GetSourceCode(url);
+System.Console.WriteLine(document);

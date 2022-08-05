448ddfa2b976ea7c958a8a7606fbaf3067d8df51
Emre Küçük <emrekucuk74@hotmail.com>
2022-08-05 10:06:39 +0300

    FindSubdomain: Created New Project

---
+++ challenges/04-find-subdomain/Program.cs
@@ -0,0 +1,7 @@
+﻿string url = "https://web.emrekucuk.com.tr";
+
+var parts = url.Split('/');
+var domain = parts[2];
+var domainParts = domain.Split('.');
+
+System.Console.WriteLine($"subdomain: {domainParts[0]}");
